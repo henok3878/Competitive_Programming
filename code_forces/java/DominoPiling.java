e2476c44646807e2e5972b071071e661a2097794
henok3878 <henokwondimu3878@gmail.com>
2021-12-09 15:56:42 +0300

    domino piling solution

--- code_forces/java/DominoPiling.java
+++ code_forces/java/DominoPiling.java
@@ -1,5 +1,24 @@
 package code_forces.java;
 
+import java.util.Scanner;
+
 public class DominoPiling {
+
+    public int solution(int m, int n){
+        return (m*n) / 2;
+    }
+
+
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        DominoPiling dominoPiling = new DominoPiling();
+        int maxDominos = dominoPiling.solution(m, n);
+        System.out.println(maxDominos);
+
+
+    }
     
 }
