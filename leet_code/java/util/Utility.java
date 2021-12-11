cacd9029b170ef8c5d7ca16b8892c0f6225528e9
henok3878 <henokwondimu3878@gmail.com>
2021-12-11 11:30:34 +0300

    add utility function to print array

---
+++ leet_code/java/util/Utility.java
@@ -0,0 +1,14 @@
+package leet_code.java.util;
+
+public class Utility {
+
+
+    public static void printArray(int[] arr){
+        for(int elem : arr){
+            System.out.print(elem+", ");
+        }
+        System.out.println();
+    }
+
+    
+}
