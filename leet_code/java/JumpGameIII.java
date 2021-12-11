675af445d74ed12b58ea8b5d0bc996af41e0f179
henok3878 <henokwondimu3878@gmail.com>
2021-12-11 23:42:42 +0300

    leetcode jump gameIII solution

--- leet_code/java/JumpGameIII.java
+++ leet_code/java/JumpGameIII.java
@@ -1,35 +1,52 @@
 package leet_code.java;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JumpGameIII {
 
     boolean canReach(int[] arr, int start){
-        if(start < 0 || start >= arr.length){
-            return false;
-        }
-        else if(arr[start] == 0){
-            return true;
-        }
-        else{
-            int nextJump1 = start + arr[start];
-            int nextJump2 = start - arr[start];
+        Set<Integer> alreadyVisited = new HashSet<>();
+        return canReachHelper(arr, start, alreadyVisited);
+    }
 
-            boolean firstJumpResult = canReach(arr, nextJump1);
-            boolean secondJumpResult = canReach(arr, nextJump2);
-            if(nextJump1 < 0 || nextJump1 >= arr.length){
-                    firstJumpResult = false;
-            }else if(nextJump2 < 0 || nextJump2 >= arr.length){
-                secondJumpResult = false;
+                    
+
+        boolean canReachHelper(int[] arr, int index, Set<Integer> alreadyVisited){
+            if(index < 0 || index >= arr.length){
+                return false;
             }
-            return firstJumpResult || secondJumpResult;
+            else if(arr[index] == 0){
+                return true;
+            }
+            else{
+                int nextJump1 = index + arr[index];
+                int nextJump2 = index - arr[index];
+                if(alreadyVisited.contains(nextJump1)){
+                    return false;
+                }
+                boolean firstJumpResult = canReachHelper(arr, nextJump1,alreadyVisited);
+                alreadyVisited.add(nextJump1);
+                if(!firstJumpResult){
+                    if(alreadyVisited.contains(nextJump2)){
+                        return false;
+                    }
+                    boolean secondJumpResult = canReachHelper(arr, nextJump2,alreadyVisited);
+                    alreadyVisited.add(nextJump2);
+                    return secondJumpResult;
+                }else{
+                    return true;
+                } 
         }
     }
 
 
 
+
     public static void main(String[] args){
 
-        int[] arr = {4,2,3,0,3,1,2};
-        int start = 5;
+        int[] arr = {3,0,2,1,2};
+        int start = 2;
         JumpGameIII jumpGameIII = new JumpGameIII();
         System.out.println(jumpGameIII.canReach(arr, start));
 
