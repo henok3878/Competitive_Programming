ab8c1fb601656b476cc84ef645fad00a302f1a39
henok3878 <henokwondimu3878@gmail.com>
2021-12-14 12:04:53 +0300

    Leetcode arr with elem != avg of its neighbors sol

---
+++ leet_code/java/ArrayWithElementsNotEqualToAverageOfNeighbors.java
@@ -0,0 +1,26 @@
+package leet_code.java;
+
+import java.util.Arrays;
+
+import leet_code.java.util.Utility;
+
+public class ArrayWithElementsNotEqualToAverageOfNeighbors {
+    public int[] rearrangeArray(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i  < nums.length; i+= 2){
+            int temp = nums[i];
+            nums[i] = nums[i-1];
+            nums[i-1] = temp;
+        }
+        return nums;
+    }
+
+
+    public static void main(String[] args){
+        ArrayWithElementsNotEqualToAverageOfNeighbors arrayWithElementsNotEqualToAverageOfNeighbors = new ArrayWithElementsNotEqualToAverageOfNeighbors();
+        int[] nums = {1,2,3,4,5};
+        int[] ans = arrayWithElementsNotEqualToAverageOfNeighbors.rearrangeArray(nums);
+        System.out.println("Result: ");
+        Utility.printArray(ans);
+    }
+}
