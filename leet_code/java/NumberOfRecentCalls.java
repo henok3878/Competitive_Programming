59deb4904e91f6e8f99789fc3bb0a2d690b4ad36
henok3878 <henokwondimu3878@gmail.com>
2021-12-21 18:57:24 +0300

    Leetcode number of recent calls prob solution

---
+++ leet_code/java/NumberOfRecentCalls.java
@@ -0,0 +1,27 @@
+package leet_code.java;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class  NumberOfRecentCalls {
+
+    Queue<Integer> queue;
+
+    public NumberOfRecentCalls() {
+        queue = new LinkedList();
+    }
+    
+    public int ping(int t) {
+        queue.add(t);
+        while(queue.peek() < t - 3000){
+            queue.poll();
+        }
+        return queue.size();
+        
+    }
+
+    public static void main(String[] args){
+        
+    }
+    
+}
