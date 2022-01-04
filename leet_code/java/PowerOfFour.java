b88e461b4d8fc6e4770b102923229011b0a49d6b
henok3878 <henokwondimu3878@gmail.com>
2022-01-04 23:41:19 +0300

    Leetcode power of four prob solution

---
+++ leet_code/java/PowerOfFour.java
@@ -0,0 +1,20 @@
+package leet_code.java;
+
+public class PowerOfFour {
+    public boolean isPowerOfFour(int n) {
+        
+        if(n < 1){
+            return false;
+        }
+        else if(n == 1){
+            return true;
+        }
+        else if(n%4 != 0){
+                return false;
+        }
+        else{
+            return isPowerOfFour(n/4);
+        }
+        
+    }
+}
