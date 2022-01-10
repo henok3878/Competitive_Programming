bdd83b8158d93bce1b3171685da6699b5ce778fc
henok3878 <henokwondimu3878@gmail.com>
2022-01-10 15:30:27 +0300

    Leetcode merge two sorted lists recursive soln

--- leet_code/java/MergeTwoSortedLists.java
+++ leet_code/java/MergeTwoSortedLists.java
@@ -2,31 +2,26 @@ package leet_code.java;
 
 public class MergeTwoSortedLists {
 
-    //Iterative solution
-    
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        ListNode ans = new ListNode();
-        ListNode temp = ans;
-        int val1 = Integer.MAX_VALUE;
-        int val2 = Integer.MAX_VALUE;
-        while(list1 != null || list2 != null){
-            val1 = list1 == null ? Integer.MAX_VALUE : list1.val;
-            val2 = list2 == null ? Integer.MAX_VALUE : list2.val;
-            
-            if(val1 <= val2){
-                temp.next= list1;
-                list1 = list1.next;
-            }
-            else{
-                temp.next =  list2;
-                list2 = list2.next;
-            }
-            temp = temp.next;
+    //Recursive solution
+
+   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       
+        if(list1 == null){
+             return list2;       
+        }else if(list2 == null){
+            return list1;
         }
-        return ans.next;
-        
+        else{
+            
+            if(list1.val <= list2.val){
+                ListNode res = mergeTwoLists(list1.next,list2);
+                list1.next = res;
+                return list1;
+            }
+            ListNode res= mergeTwoLists(list1,list2.next);
+            list2.next = res;
+            return list2;
+        }
+       
     }
 }
