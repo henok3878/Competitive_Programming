768fa31a022a2c68a93fd3e3c8718053b59d90ac
henok3878 <henokwondimu3878@gmail.com>
2022-01-14 11:20:18 +0300

    adds recursive impln for leetcode prob add 2 num

--- leet_code/java/AddTwoNumbers.java
+++ leet_code/java/AddTwoNumbers.java
@@ -1,5 +1,8 @@
 package leet_code.java;
 
+
+//Iterative Solution
+
 public class AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -68,4 +71,26 @@ class ListNode {
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+// Recursive solution
+class AddTwoNumbers2{
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return addTwoNumbersHelper(l1,l2,0);
+    }
+    
+    private ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry){
+        
+        if(l1 == null && l2 == null && carry == 0){
+            return null;
+        }
+        int num1 = l1 == null ? 0 : l1.val;
+        int num2 = l2 == null ? 0 : l2.val;
+                
+        
+        l1 = l1 == null ? null : l1.next;
+        l2 = l2 == null ? null : l2.next;
+        ListNode sum = new ListNode((num1 + num2 + carry)%10,addTwoNumbersHelper(l1,l2,(num1+num2 + carry)/10));
+        return sum;
+    }
 }
