05af2cafada50c574ea5c8c93bdeb16ad366dc09
henok3878 <henokwondimu3878@gmail.com>
2022-01-16 14:42:14 +0300

    refactor to sol to a single pass

--- leet_code/java/CopyListWithRandomPointer.java
+++ leet_code/java/CopyListWithRandomPointer.java
@@ -4,26 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CopyListWithRandomPointer {
+    
+
     public Node copyRandomList(Node head) {
+        if(head == null) return head;
+        
         Map<Node, Node> oldToNew = new HashMap();
-        
         Node temp = head;
-        Node newHead = new Node(-1);
-        Node dummy = newHead;
+        
+        oldToNew.put(head, new Node(head.val));
         
         while(temp != null){
-            dummy.next = new Node(temp.val);
-            dummy = dummy.next;
-            oldToNew.put(temp,dummy);
-            temp = temp.next;
-        }
-        temp = head;
-        while(temp != null){
-            oldToNew.get(temp).random = oldToNew.get(temp.random);
+            
+            if(!oldToNew.containsKey(temp)){
+                oldToNew.put(temp ,new Node(temp.val));        
+            }
+            if(temp.next != null){
+                 if(!oldToNew.containsKey(temp.next)){
+                    oldToNew.put(temp.next, new Node(temp.next.val));
+                 }
+                 oldToNew.get(temp).next = oldToNew.get(temp.next); 
+
+            }
+            if(temp.random != null){
+                if(!oldToNew.containsKey(temp.random)){
+                    oldToNew.put(temp.random, new Node(temp.random.val));
+                }
+                oldToNew.get(temp).random = oldToNew.get(temp.random); 
+            }
             temp = temp.next;
         }
         
-        return newHead.next;
+        return oldToNew.get(head);
         
     }
     
