75ec0e92cafcbf9d6cd774cdb710228dec16a8bb
henok3878 <henokwondimu3878@gmail.com>
2022-01-16 14:41:11 +0300

    fix syntax error

--- leet_code/java/CopyListWithRandomPointer.java
+++ leet_code/java/CopyListWithRandomPointer.java
@@ -28,3 +28,15 @@ public class CopyListWithRandomPointer {
     }
     
 }
+
+class Node{
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
