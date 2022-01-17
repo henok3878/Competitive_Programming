ac4073f1f8d70a55deec6e0072158cd2bd0d020f
henok3878 <henokwondimu3878@gmail.com>
2022-01-17 14:22:17 +0300

    Leetcode remove nth node from ll 1 pass recur sol

--- leet_code/java/RemoveNthNodeFromEndOfList.java
+++ leet_code/java/RemoveNthNodeFromEndOfList.java
@@ -4,27 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RemoveNthNodeFromEndOfList {
+   
+    int size = 0;
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        
-        
-        List<ListNode> linkedList = new ArrayList();
-        while(head != null){
-            linkedList.add(head);
-            head = head.next;
-        }
-        if(linkedList.size() < 2){
-            return null;
-        }
-        int remIdx = linkedList.size() - n;
-        if(remIdx - 1 < 0){
-          return  linkedList.get(1);
-        }
-        ListNode prev = linkedList.get(remIdx - 1);
-        ListNode remNode = linkedList.get(remIdx);
-        prev.next = remNode.next;
-        remNode.next = null;
-        
-        return linkedList.get(0);
+        return removeNthFromEndHelper(head,0,n);
+    }
+    
+    private ListNode removeNthFromEndHelper(ListNode head, int count, int n){
+       
+        if(head == null)return head;
+        size++;
+        head.next = removeNthFromEndHelper(head.next,count + 1, n);
+        if(count == (size - n))   return head.next;
+        return head;
     }
     
 }
