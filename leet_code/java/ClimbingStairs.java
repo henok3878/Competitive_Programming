e2d85ecfc06479f54fdf07e0e802bb3ae6f78324
henok3878 <henokwondimu3878@gmail.com>
2022-02-05 23:24:57 +0300

    climbing stairs optimized dp solution

--- leet_code/java/ClimbingStairs.java
+++ leet_code/java/ClimbingStairs.java
@@ -2,12 +2,10 @@ package leet_code.java;
 
 public class ClimbingStairs {
     public int climbStairs(int n) {
-        int[] ways = new int[n + 1];
-        ways[n-1] = 1;
-        ways[n] = 1;
-        for(int i = ways.length - 3; i >= 0; i--){
-            ways[i] = ways[i + 1] + ways[i + 2];
+        int a = 1, b = 1;
+        for(int i = n - 2; i >= 0; i--){
+            int t = a; a = a + b; b = t;            
         }
-        return ways[0];
+        return a;
     }
 }
