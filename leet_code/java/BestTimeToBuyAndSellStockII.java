31a35c6a073531d592810b0ae510845567dc38ae
henok3878 <henokwondimu3878@gmail.com>
2022-02-09 19:44:06 +0300

    Besttime to buy and sell stockII dp solution

--- leet_code/java/BestTimeToBuyAndSellStockII.java
+++ leet_code/java/BestTimeToBuyAndSellStockII.java
@@ -4,18 +4,9 @@ public class BestTimeToBuyAndSellStockII {
 
     public int maxProfit(int[] prices) {
         
-        int profit = 0;
-        int buy = -1;
-        for(int i = 0; i < prices.length; i++){
-           int left = ((i - 1) >= 0) ? prices[i-1] : Integer.MAX_VALUE;
-           int right = ((i + 1) < prices.length) ? prices[i+1] : Integer.MIN_VALUE;
-           if(buy == -1 && prices[i] <= left && prices[i] < right) buy = prices[i];
-           else if(buy != -1 && prices[i] >= left && prices[i] > right){
-                profit += prices[i] - buy; 
-                buy = -1;
-           }
-        }
-        return profit;
-    }
+        int[] dp = new int[prices.length];
+        for(int i = 1; i < dp.length; i++) dp[i] = Math.max(dp[i-1], dp[i-1] + (prices[i] - prices[i-1]));
+        return dp[prices.length - 1];
+     }
     
 }
