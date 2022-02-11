307b88874fd9745224228324d5286dfe0716cb5a
henok3878 <henokwondimu3878@gmail.com>
2022-02-11 19:45:39 +0300

    max consecutive ones cleaner version

--- leet_code/java/MaxConsecutiveOnesIII.java
+++ leet_code/java/MaxConsecutiveOnesIII.java
@@ -5,12 +5,15 @@ public class MaxConsecutiveOnesIII {
         
         int i = 0; 
         int longest = 0;
+        int zeros = 0;
         for(int j = 0; j < nums.length; j++){
-            if(nums[j] == 0) k--;
-            if (k < 0 && nums[i++] == 0) k++;
-            longest = Math.max(longest,j - i + 1);  
+            if (nums[j]==0) zeros++;
+            if (zeros > k) {
+                if (nums[i]==0) zeros--;
+                i++;
+            }
+            if (zeros <= k) longest = Math.max(longest, j -i +1 );
         }
-        
         return longest;
     }
 }
