2b607c886005d9663beb85e55d20d1d0f2d76475
henok3878 <henokwondimu3878@gmail.com>
2022-02-20 11:40:08 +0300

    Leetcode set matrix zeros O(1) space solution

--- leet_code/java/SetMatrixZeroes.java
+++ leet_code/java/SetMatrixZeroes.java
@@ -1,27 +1,31 @@
 package leet_code.java;
 
-import java.util.HashSet;
-import java.util.Set;
 
 public class SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rows = new HashSet<>();
-        Set<Integer> cols = new HashSet<>();
+        int firstCol = 1; int firstRow = 1;
+        
         for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
+            for(int j = 0; j < matrix[0].length;j ++){
                 if(matrix[i][j] == 0){
-                    rows.add(i); 
-                    cols.add(j);
+                    if(j == 0) firstCol = 0;
+                    if(i == 0) firstRow = 0;
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
+                
             }
         }
-         
-         for(int i = 0;i < matrix.length; i++){
-             for(int j = 0;j < matrix[0].length; j++){
-                 if(rows.contains(i) || cols.contains(j)){
-                     matrix[i][j] = 0;
-                 }
-             }
-         }
-     }
+        
+        
+        for(int i = matrix.length - 1;i >= 0; i--){
+            for(int j = matrix[0].length - 1;j >= 0; j--){
+                if(i == 0 || j == 0){
+                    if(i == 0 && firstRow == 0) matrix[i][j] = 0;
+                    if(j == 0 && firstCol == 0) matrix[i][j] = 0;
+                }
+                else if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+        
+    }
 }
