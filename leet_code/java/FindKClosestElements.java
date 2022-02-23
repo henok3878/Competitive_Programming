29c5f04e0132d2635d7c9cb85e9a3977b2cfee4f
henok3878 <henokwondimu3878@gmail.com>
2022-02-23 20:50:35 +0300

    Leetcode find k closest elem solution

---
+++ leet_code/java/FindKClosestElements.java
@@ -0,0 +1,36 @@
+package leet_code.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindKClosestElements {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int n = arr.length;
+        // O(log(n))
+        int mid = binarySearch(arr,x,0,n - 1);
+        int left = (mid - k < 0) ? 0 : mid - k;
+        int right = (mid + k >= n) ? n - 1 : mid + k;
+        
+        //O(k)
+        while(right - left + 1 != k){
+            if(Math.abs(arr[right] - x) < Math.abs(arr[left] - x)) left++;
+            else right--;
+        }
+        List<Integer> ans = new AsrrayList();
+        //O(k)
+        for(int i = left; i <= right; i++) ans.add(arr[i]);
+        
+        return ans;
+    }
+    
+    // O(log(n))
+    private int binarySearch(int[] arr,int num, int st, int end){
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+            if(arr[mid] > num) end = mid - 1;
+            else if(arr[mid] < num) st = mid + 1;
+            else return mid;
+        }
+        return st;
+    }
+}
